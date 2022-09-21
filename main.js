625497309c79a54cef6ee37a56d7a10844977ba9
DevDachan <dachan9524@gmail.com>
2022-09-21 21:15:20 +0900

    Create Project

---
+++ main.js
@@ -0,0 +1,58 @@
+var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+var server = require('./lib/server.js');
+
+
+
+var session = require('express-session');
+
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+
+
+
+//-------------------------- admin.js  ------------------------------------
+app.post('/', function(request, response){
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  server.main(request,response,queryData);
+});
+
+app.get('/', function(request, response){
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  server.main(request,response,queryData);
+});
+
+
+app.post('/admin_login', function(request, response){
+  server.login(request, response);
+})
+app.get('/admin_login', function(request, response){
+  server.login(request, response);
+})
+
+
+app.post('/server_register', function(request, response){
+  server.register(request, response);
+})
+
+app.get('/server_register', function(request, response){
+  server.register(request, response);
+})
+
+app.post('/server_history', function(request, response){
+  server.history(request, response);
+})
+app.get('/server_history', function(request, response){
+  server.history(request, response);
+})
+
+
+
+app.listen(3000, function(){
+  console.log('conneted 3000 port!!');
+});
